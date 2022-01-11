ff2cbac2f0529bbcb0781b7eb1aa405a4c38ac31
hgh188 <45268614+hgh188@users.noreply.github.com>
2022-01-11 13:55:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,23 +10,8 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-
 # readd cpufreq for aarch64 & Change to system
 sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
 sed -i 's/services/system/g'  package/lean/luci-app-cpufreq/luasrc/controller/cpufreq.lua
-# autocore
-sed -i 's/DEPENDS:=@(.*/DEPENDS:=@(TARGET_bcm27xx||TARGET_bcm53xx||TARGET_ipq40xx||TARGET_ipq806x||TARGET_ipq807x||TARGET_mvebu||TARGET_rockchip||TARGET_armvirt) \\/g' package/lean/autocore/Makefile
-
-# openssh
-sed -i '175i\	--with-sandbox=rlimit \\' feeds/packages/net/openssh/Makefile
-
-
-
-
-
-./scripts/feeds update -a
-./scripts/feeds install -a
